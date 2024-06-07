b12d94c0ada93e2f585882176064c23c5c9d5d6b
vanshikaya <vanshikayadav2005@gmail.com>
2024-06-07 17:23:50 +0530

    Add files via upload

---
+++ Project (1).py
@@ -0,0 +1,15 @@
+#[project]
+
+"""
+a) Build a Temperature Conversion Application
+Build a temperature conversion application that converts temperature from
+degree Fahrenheit to degree Celcius and vice versa, using the python tkinter module.
+
+
+
+
+
+
+
+
+""" 
